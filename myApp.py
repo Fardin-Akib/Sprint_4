f07da3b8c06b5cbb4aa6f5baa16f7c5bd0076889
Fardin-Akib <akib3000@gmail.com>
2025-06-19 00:23:49 -0400

    Add Streamlit app, dataset, and config files for deployment

---
+++ myApp.py
@@ -0,0 +1,35 @@
+import streamlit as st 
+import pandas as pd
+import plotly.express as px
+
+st.header("Sprint Four")
+st.subheader("Vehicle Dashboard")
+st.write("In this project you will see a historgram and scatterplot I quickly made from the 'vehicle_us.csv' dataset." \
+"In the Histogram you will see the odometer reading for each brand of cars sold. In the scatterplot you will see a correlation between" \
+"model year of a car and the price it was sold at.")
+
+df = pd.read_csv(r"C:\Users\farki\Desktop\Data Science\Sprint_Four\Sprint_4\vehicles_us.csv")
+df = df.dropna()
+
+year_price_scatter = px.scatter(df, x="model_year", y="price", title="Price vs Model Year")
+year_price_scatter.update_layout(
+    xaxis_title="Model Year",
+    yaxis_title="Price",
+    title= "Price vs Model Year"
+)
+scatter_button = st.button("Scatter")
+
+if scatter_button:
+    st.plotly_chart(year_price_scatter)
+
+brand_vs_odometer = px.histogram(df, x="model" , y="odometer", title= "Distance Travel via Brand")
+brand_vs_odometer.update_layout(
+    xaxis_title= "Model",
+    yaxis_title= "Odometer",
+    title= "Brand and Odometer Correlation"
+)
+histogram_button = st.button("Histogram")
+
+if histogram_button:
+    st.plotly_chart(brand_vs_odometer)
+
